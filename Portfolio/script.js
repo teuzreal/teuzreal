320370b6aaf2b84fb3fd5b01a874e193c618af32
Matheus Castro <matheus.castro09@aluno.ifce.edu.br>
2025-08-28 11:02:02 -0300

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,15 @@
+
+function enviarWhats(event) {
+    event.preventDefault();
+
+    const nome = document.getElementById('nome').value;
+    const mensagem = document.getElementById('mensagem').value;
+    const telefone = '5585986222412';
+
+    const texto = `Olá! Me chamo ${nome}, ${mensagem}`;
+    const msgFormatada = encodeURIComponent(texto);
+
+    const url = `https://wa.me/${telefone}?text=${msgFormatada}`;
+
+    window.open(url, '_blank');
+}
